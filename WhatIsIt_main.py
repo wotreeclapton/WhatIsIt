6f99a436738f27dbc8150ea183801c9d5c8c0c5e
wotreeclapton <58225044+wotreeclapton@users.noreply.github.com>
2020-09-21 14:34:50 +0700

    Add files via upload

---
+++ WhatIsIt_main.py
@@ -0,0 +1,8 @@
+#! python 3
+'''
+WHAT IS IT APP LAUNCHER developed by Mr Steven J walden
+    Sept. 2020
+    SAMROIYOD, PRACHUAP KIRI KHAN, THAILAND
+[See License.txt file]
+'''
+import pygame as pg
